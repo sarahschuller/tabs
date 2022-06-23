f46288abc19c51b9e2e2d5f5f93b3844e8216c79
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-06-23 19:47:13 -0400

    hide all tabs, mark all tabs as unselected, mark the clicked tab as selected, find the associate tab panel and show it

--- tabs.js
+++ tabs.js
@@ -1,15 +1,23 @@
 const tabs = document.querySelector('.tabs');
-const tabButtons = tabs.querySelectorAll('[roll="tab"]');
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
 const tabPanels = tabs.querySelectorAll('[role="tabpanel"]');
 
-function handleTabClick(e) {
+function handleTabClick(event) {
     // hide all tab panels
-    tabPanels.forEach(function(panel) {
-        panel.hidden = true;
-    })
+    tabPanels.forEach(panel => {
+      panel.hidden = true;
+    });
     // mark all tabs as unselected
+    tabButtons.forEach(tab => {
+        tab.setAttribute('aria-selected', false);
+    });
     // mark the clicked tab as selected
+    event.currentTarget.setAttribute('aria-selected', true);
     // find the associated tab panel and show it
+    const id = event.currentTarget.id;
+    const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
+    console.log(tabPanel);
+    tabPanel.hidden = false;
 }
 
 tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
